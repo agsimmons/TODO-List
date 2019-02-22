bb92f700cb8b87847eb4f7f257e9aa827819e026
Andrew Simmons <agsimmons0@gmail.com>
2019-02-21 21:41:42 -0500

    Initial provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -66,4 +66,6 @@ Vagrant.configure("2") do |config|
   #config.vm.provision "shell", inline: <<-SHELL
   #
   #SHELL
+
+  config.vm.provision :shell, path: "provision.sh"
 end
---
+++ provision.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Update packages
+apt-get update
+
+# === Apache ===
+# Install Apache
+apt-get install -y apache2
+
+# Set up synced HTML folder
+if ! [ -L /var/www/html ]; then
+	rm -rf /var/www/html
+	ln -fs /vagrant/html /var/www/html
+fi
+# ==============
+
+# === MariaDB / MySQL ===
+apt-get install -y mariadb-server mariadb-client
+
+# TODO: Configure MariaDB
+# =======================
