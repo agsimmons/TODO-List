1968386f9987840a9d2ee541c511e9c3be022c08
Andrew Simmons <agsimmons0@gmail.com>
2019-03-19 12:54:58 -0400

    Create login and register skeletons

---
+++ html/php/login.php
@@ -0,0 +1,16 @@
+<?php
+
+    $mysql_host = "localhost";
+    $mysql_user = "todouser";
+    $mysql_pass = "todouserpassword";
+    $mysql_db = "todo";
+
+    $conn = new mysqli($mysql_host, $mysql_user, $mysql_pass);
+
+    // TODO: Implement login
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+?>
