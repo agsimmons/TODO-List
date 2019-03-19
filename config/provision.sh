5adef5fde28ef3c04978e8ee38e29f5ab89673a8
Andrew Simmons <agsimmons0@gmail.com>
2019-03-19 13:13:54 -0400

    Fix typo and add comments

--- config/provision.sh
+++ config/provision.sh
@@ -18,13 +18,17 @@ fi
 # === MariaDB / MySQL ===
 apt-get install -y mariadb-server mariadb-client
 
-# Configure MariaDB
+# Create database
 mysql -e "DROP DATABASE IF EXISTS todo;"
 mysql -e "CREATE DATABASE todo CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+
+# Create user
 mysql -e "CREATE USER 'todouser' IDENTIFIED BY 'todouserpassword';"
 mysql -e "GRANT USAGE ON *.* TO 'todouser'@localhost IDENTIFIED BY 'todouserpassword';"
 mysql -e "GRANT ALL PRIVILEGES ON `todo`.* TO 'todouser'@localhost;"
-mysql -e "FLUSH PRIVELEGES;"
+mysql -e "FLUSH PRIVILEGES;"
+
+# Load database schema
 mysql todo < /vagrant/config/schema.sql
 # =======================
 
