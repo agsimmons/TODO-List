aff5dfe35c2ddd9e884ba5bc0c3951d695bb9199
Andrew Simmons <agsimmons0@gmail.com>
2019-03-19 13:14:31 -0400

    Drop database user upon provisioning

--- config/provision.sh
+++ config/provision.sh
@@ -23,6 +23,7 @@ mysql -e "DROP DATABASE IF EXISTS todo;"
 mysql -e "CREATE DATABASE todo CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 
 # Create user
+mysql -e "DROP USER IF EXISTS 'todouser'@localhost;"
 mysql -e "CREATE USER 'todouser' IDENTIFIED BY 'todouserpassword';"
 mysql -e "GRANT USAGE ON *.* TO 'todouser'@localhost IDENTIFIED BY 'todouserpassword';"
 mysql -e "GRANT ALL PRIVILEGES ON `todo`.* TO 'todouser'@localhost;"
