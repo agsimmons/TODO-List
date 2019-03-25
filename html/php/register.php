8a4809653b9c054b19c2c9f0bc3c30d3e6e80023
Andrew Simmons <agsimmons0@gmail.com>
2019-03-25 11:44:53 -0400

    Implement basics of login and register

--- html/php/register.php
+++ html/php/register.php
@@ -1,17 +1,31 @@
 <?php
 
+    // Initialize database connection variables
     $mysql_host = "localhost";
     $mysql_user = "todouser";
     $mysql_pass = "todouserpassword";
     $mysql_db = "todo";
 
-    $conn = new mysqli($mysql_host, $mysql_user, $mysql_pass);
+    // Create connection to database
+    $conn = new mysqli($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
 
-    // TODO: Implement register
-    // Check connection
+    // Check connection to database
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        die("ERROR: Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
+
+    # Parse parameters from form
+    $username = $_POST["username"];
+    $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+
+    $sql_register = "INSERT INTO user(username, password) VALUES ('".$username."', '".$password."');";
+
+    if (mysqli_query($conn, $sql_register)) {
+        echo "User created successfully";
+    } else {
+        echo "ERROR: " . mysqli_error($conn);
+    }
+
+    $conn->close();
 
 ?>
