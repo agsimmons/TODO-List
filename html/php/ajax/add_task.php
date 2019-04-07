5ceedada5d1669cb03ee251689a084110f613936
Andrew Simmons <agsimmons0@gmail.com>
2019-04-07 13:45:16 -0400

    Redirect back to main when adding task

--- html/php/ajax/add_task.php
+++ html/php/ajax/add_task.php
@@ -16,8 +16,9 @@ if ($conn->connect_error) {
 
 // TODO: Make this not vulnerable to SQL injection
 $query = "INSERT INTO task (fk_user, name, due_date, description) VALUES (" . $_SESSION["user_id"] . ", '" . $_POST["task_name"] . "', '" . $_POST["task_due_date"] . "', '" . $_POST["task_description"] . "');";
-echo $query;
 $result = mysqli_query($conn, $query);
+
+header('Location: /main.php');
 
 mysqli_close($conn);
 
