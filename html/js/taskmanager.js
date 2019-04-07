42f452606b60b6c5e99324eaf9614c9972565dd6
Andrew Simmons <agsimmons0@gmail.com>
2019-04-07 12:33:15 -0400

    Implement complete task function

--- html/js/taskmanager.js
+++ html/js/taskmanager.js
@@ -45,3 +45,18 @@ function deleteTask(id) {
     });
 
 }
+
+function completeTask(id) {
+
+    $.ajax({
+        type: 'POST',
+        url: '/php/ajax/complete_task.php',
+        data: {
+            task_id: id,
+        },
+        success: function(response) {
+            getIncompleteTasks();
+        }
+    });
+
+}
